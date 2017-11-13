a7ab75e36d4ca4f156c5940850450adc0cb5f9e7
Domse <dominik.martens@web.de>
2017-11-13 21:22:22 +0100

    added qick'n'dirty database actions

--- mqttService.py
+++ mqttService.py
@@ -1,22 +1,37 @@
-#!/bin/python
+#!/bin/python3
 
 import paho.mqtt.client as mqtt
 import requests,json,time
 import credentials
+import mysql.connector as mariadb
 
-MQTT_TOPIC_BASE = "A0:20:A6:14:D7:7A"
+MQTT_TOPIC_BASE = "/A0:20:A6:14:D7:7A"
+MAC = "A020A614D77A"
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
 def on_message(client, userdata, msg):
-    print("recived message \"" + str(msg.payload.decode('utf-8')) + "\" in " + str(msg.topic))
-    if str(msg.payload.decode('utf-8')) =="1301970":
-            client.publish(str(msg.topic)+"/state", str('s'))
-    else :
-            client.publish(str(msg.topic)+"/state", str("l"))
+    token = str(msg.payload.decode('utf-8'))
+    top = str(msg.topic)
+    print("recived message \"" + token + "\" in " + top)
+    cursor.execute("SELECT `relay`.`mac` FROM `token` INNER JOIN `user` ON(`token`.`user` = `user`.`id`) INNER JOIN `user_relay` ON(`user`.`id` = `user_relay`.`user`) INNER JOIN `relay` ON(`user_relay`.`relay` = `relay`.`id`) INNER JOIN `reader_relay` ON(`relay`.`id` = `reader_relay`.`relay`) INNER JOIN `reader` ON(`reader_relay`.`reader` = `reader`.`id`) WHERE `token`.`id` = '%s' AND `reader`.`mac` = '%s'" % (token,MAC))
+    sucess = False
+    for mac in cursor:
+        if "%s"%mac == MAC:
+            client.publish(top+"/state", str('s'))
+            sucess = True
+            print("Access granted for %s"%token)
+        else:
+            client.publish(top+"/state", str('e'))
+            print("Acess denied for %s"%token)
+    if not(sucess):
+        client.publish(top+"/state", str('e'))
+        print("Acess denied for %s"%token)
 
-print(credentials.mqttBrokerURL)
+cnx = mariadb.connect(user = credentials.mariadbUser, password = credentials.mariadbPw, host =credentials.mariadbServer, database=credentials.mariadbUser) 
+cursor = cnx.cursor()
+print("MQTT-Broker: " + credentials.mqttBrokerURL)
 client = mqtt.Client()
 client.username_pw_set(credentials.mqtt_username, credentials.mqtt_password)
 client.on_connect = on_connect
