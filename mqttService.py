9b43ecaf63ec55a77f98ba46fd7ed8d3a9b18406
Simon Kurka <dev@simon.kurka.cc>
2017-12-13 00:00:17 +0100

    Python-Foo etwas hübscher und mit Doppelpunkten in MAC

--- mqttService.py
+++ mqttService.py
@@ -11,22 +11,17 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     token = str(msg.payload.decode('utf-8'))
-    top = str(msg.topic)
-    MAC=top.replace(":","").upper().replace("/","")
-    print("recived message \"" + token + "\" in " + top)
-    cursor.execute("SELECT `relay`.`mac` FROM `token` INNER JOIN `user` ON(`token`.`user` = `user`.`id`) INNER JOIN `user_relay` ON(`user`.`id` = `user_relay`.`user`) INNER JOIN `relay` ON(`user_relay`.`relay` = `relay`.`id`) INNER JOIN `reader_relay` ON(`relay`.`id` = `reader_relay`.`relay`) INNER JOIN `reader` ON(`reader_relay`.`reader` = `reader`.`id`) WHERE `token`.`id` = '%s' AND `reader`.`mac` = '%s'" % (token,MAC))
-    sucess = False
-    for mac in cursor:
-        if "%s"%mac == MAC:
-            client.publish(top+"/state", str('s'))
-            sucess = True
-            print("Access granted for %s"%token)
-        else:
-            client.publish(top+"/state", str('e'))
-            print("Acess denied for %s"%token)
-    if not(sucess):
-        client.publish(top+"/state", str('e'))
-        print("Acess denied for %s"%token)
+    topic = str(msg.topic)
+    reader = topic[1:].upper()
+    cursor.execute("SELECT `relay`.`mac`, `user`.`name`, `reader`.`name`, `relay`.`name` FROM `token` INNER JOIN `user` ON(`token`.`user` = `user`.`id`) INNER JOIN `user_relay` ON(`user`.`id` = `user_relay`.`user`) INNER JOIN `relay` ON(`user_relay`.`relay` = `relay`.`id`) INNER JOIN `reader_relay` ON(`relay`.`id` = `reader_relay`.`relay`) INNER JOIN `reader` ON(`reader_relay`.`reader` = `reader`.`id`) WHERE `token`.`id` = '%s' AND `reader`.`mac` = '%s'" % (token, reader))
+    success = False
+    for (relayMac, userName, readerName, relayName) in cursor:
+        client.publish("/%s/state" % (relayMac), str('s'))
+        success = True
+        print("Access granted for user %s on door %s with reader %s" % (userName, relayName, readerName))
+    if not(success):
+        client.publish(topic+"/state", str('e'))
+        print("Access denied for token %s" % (token))
 
 cnx = mariadb.connect(user = credentials.mariadbUser, password = credentials.mariadbPw, host =credentials.mariadbServer, database=credentials.mariadbUser) 
 cursor = cnx.cursor()
