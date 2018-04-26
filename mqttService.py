f61434207d86f9ab2d7f14e272c6f6cebffaa07d
Simon Kurka <dev@simon.kurka.cc>
2018-04-26 22:06:25 +0200

    db commit added everything solved...

--- mqttService.py
+++ mqttService.py
@@ -13,7 +13,7 @@ def on_message(client, userdata, msg):
     token = str(msg.payload.decode('utf-8'))
     topic = str(msg.topic)
     reader = topic[1:].upper()
-    cursor = cnx.cursor()
+    cursor = db.cursor()
     cursor.execute("SELECT `relay`.`id`, `relay`.`mac`, `user`.`id`, `user`.`name`, `reader`.`id`, `reader`.`name`, `relay`.`name` FROM `token` INNER JOIN `user` ON(`token`.`user` = `user`.`id`) INNER JOIN `user_relay` ON(`user`.`id` = `user_relay`.`user`) INNER JOIN `relay` ON(`user_relay`.`relay` = `relay`.`id`) INNER JOIN `reader_relay` ON(`relay`.`id` = `reader_relay`.`relay`) INNER JOIN `reader` ON(`reader_relay`.`reader` = `reader`.`id`) WHERE `token`.`id` = '%s' AND `reader`.`mac` = '%s'" % (token, reader))
     success = False
     logstr = ""
@@ -24,15 +24,16 @@ def on_message(client, userdata, msg):
         print("Access granted for user %s on door %s with reader %s" % (userName, relayName, readerName))
     if (success):
         client.publish(topic+"/state", str('s'))
-        log = cnx.cursor()
+        log = db.cursor()
         log.execute("INSERT INTO `log` (`user`,`reader`,`relay`,`result`) VALUES %s" % (logstr[1:]))
         log.close()
     else:
         client.publish(topic+"/state", str('e'))
         print("Access denied for token %s" % (token))
     cursor.close()
+    db.commit()
 
-cnx = mariadb.connect(user = credentials.mariadbUser, password = credentials.mariadbPw, host =credentials.mariadbServer, database=credentials.mariadbUser)
+db = mariadb.connect(user = credentials.mariadbUser, password = credentials.mariadbPw, host =credentials.mariadbServer, database=credentials.mariadbUser)
 print("MQTT-Broker: " + credentials.mqttBrokerURL)
 client = mqtt.Client()
 client.username_pw_set(credentials.mqtt_username, credentials.mqtt_password)
