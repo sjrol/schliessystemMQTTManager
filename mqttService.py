056eb9325c11b8a445e4f11e96a459adb3744ed3
Simon Kurka <dev@simon.kurka.cc>
2020-06-06 16:26:24 +0200

    Corona-Update

--- mqttService.py
+++ mqttService.py
@@ -22,21 +22,29 @@ def on_message(client, userdata, msg):
     cursor.execute("SELECT `relay`.`id`, `relay`.`mac`, `user`.`id`, `user`.`name`, `reader`.`id`, `reader`.`name`, `relay`.`name` FROM `token` INNER JOIN `user` ON(`token`.`user` = `user`.`id`) INNER JOIN `user_relay` ON(`user`.`id` = `user_relay`.`user`) INNER JOIN `relay` ON(`user_relay`.`relay` = `relay`.`id`) INNER JOIN `reader_relay` ON(`relay`.`id` = `reader_relay`.`relay`) INNER JOIN `reader` ON(`reader_relay`.`reader` = `reader`.`id`) WHERE `token`.`id` = '%s' AND `reader`.`mac` = '%s'" % (token, reader))
     success = False
     logstr = ""
+    _relayId = 0 # Corona
+    _userId = 0 # Corona
     for (relayId, relayMac, userId, userName, readerId, readerName, relayName) in cursor:
         client.publish("/door/%s" % (relayMac), str('s'))
         logstr += ",('%s','%s','%s','%s')" % (userId, readerId, relayId, 's')
         success = True
         print("Access granted for user %s on door %s with reader %s" % (userName, relayName, readerName))
+        _relayId = relayId # Corona
+        _userId = userId # Corona
     if (success):
         client.publish("/reader/%s" % (reader), str('s'))
         log = db.cursor()
         log.execute("INSERT INTO `log` (`user`,`reader`,`relay`,`result`) VALUES %s" % (logstr[1:]))
         log.close()
-        corona_checkout = db.cursor()
-        corona_checkout.execute("SELECT COUNT(*) FROM `log` WHERE `user` = %s AND `relay` = 1 AND `timestamp` > CURRENT_TIMESTAMP()-30" % (userId))
-        (corona_count) = corona_checkout.pop()
-        if corona_count > 1:
-            client.publish("/reader/%s" % (reader), str('p'))
+        if _relayId == 1: # Corona
+            corona_checkout = db.cursor()
+            corona_checkout.execute("SELECT COUNT(*) FROM `log` WHERE `user` = %s AND `relay` = 1 AND `timestamp` > CURRENT_TIMESTAMP()-30" % (_userId))
+            corona_row = corona_checkout.fetchone()
+            if corona_row is not None:
+                (corona_count) = corona_row
+                if corona_count > 1:
+                    client.publish("/reader/%s" % (reader), str('p'))
+            corona_checkout.close()
     else:
         client.publish("/reader/%s" % (reader), str('e'))
         log = db.cursor()
